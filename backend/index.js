a816de217c1fc33bc84a841be82f87cabebfc856
takshch <takshch089@gmail.com>
2022-07-25 22:05:05 +0530

    Adds express and basic route setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 const config = require('config');
+const http = require('http');
+const app = require('./src/app');
 
 const PORT = config.get('port');
 
 if (!PORT) {
   console.log('PORT is not available in config');
 }
+
+const server = http.createServer(app);
+
+function onListening() {
+  console.log(`Server started listening on port ${PORT}`);
+}
+
+function onError() {
+  console.log('error');
+}
+
+server.on('listening', onListening);
+server.on('error', onError);
+
+server.listen(PORT);
