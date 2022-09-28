9a19a1aca82f6adf64100a3c01c54acebb6dbfce
takshch <takshch089@gmail.com>
2022-09-28 18:20:01 +0530

    Makes Logo text medium & large

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <div className="flex items-center px-5 py-2 bg-indigo-600 text-white border-none">
-      <span className="mr-auto">Pahachaan</span>
+      <span className="mr-auto font-medium text-lg">Pahachaan</span>
       <nav>
         <ul className="flex items-center gap-6">
           <li>Codes</li>
