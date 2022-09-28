17adc40f72e1f056f66ba78e57d634bc5f8ee37d
takshch <takshch089@gmail.com>
2022-09-28 17:32:45 +0530

    Adds IdentityCard component and fixes next\babel error

---
+++ frontend/components/IdentityCard.tsx
@@ -0,0 +1,17 @@
+interface IdentityCardProps {
+  imageUrl: string;
+  name: string;
+}
+
+function IdentityCard({ imageUrl, name }: IdentityCardProps) {
+  return (
+    <div className="inline-flex flex-col max-w-fit px-3 py-2 border border-slate-300 rounded hover:shadow-md">
+      <div className="w-24 mb-1.5">
+        <img src={imageUrl} alt={name} />
+      </div>
+      <div className="text-center text-sm uppercase text-slate-800">{name}</div>
+    </div>
+  );
+}
+
+export default IdentityCard;
