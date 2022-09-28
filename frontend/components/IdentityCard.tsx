1f9434f160a928768e6d1c075ae0f3ccbd9b13fd
takshch <takshch089@gmail.com>
2022-09-28 17:49:16 +0530

    Adds heading for identity cards

--- frontend/components/IdentityCard.tsx
+++ frontend/components/IdentityCard.tsx
@@ -6,7 +6,7 @@ interface IdentityCardProps {
 function IdentityCard({ imageUrl, name }: IdentityCardProps) {
   return (
     <div className="inline-flex flex-col max-w-fit px-3 py-2 border border-slate-300 rounded hover:shadow-md">
-      <div className="w-24 mb-1.5">
+      <div className="w-32 mb-1.5">
         <img src={imageUrl} alt={name} />
       </div>
       <div className="text-center text-sm uppercase text-slate-800">{name}</div>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,13 +3,17 @@ import IdentityCard from '../components/IdentityCard';
 
 const Home: NextPage = () => {
   const identityCards = new Array(100).fill({
-    imageUrl: 'https://www.investopedia.com/thmb/KfGSwVyV8mOdTHFxL1T0aS3xpE8=/1148x1148/smart/filters:no_upscale()/qr-code-bc94057f452f4806af70fd34540f72ad.png',
-    name: 'qr code 1'
+    imageUrl:
+      'https://www.investopedia.com/thmb/KfGSwVyV8mOdTHFxL1T0aS3xpE8=/1148x1148/smart/filters:no_upscale()/qr-code-bc94057f452f4806af70fd34540f72ad.png',
+    name: 'qr code 1',
   });
 
   return (
-    <div className="container">
+    <div className="w-full">
       <div className="px-4 py-8">
+        <h1 className="text-2xl text-center font-medium mt-2 mb-5">
+          Your Identities
+        </h1>
         <div className="flex flex-wrap gap-x-7 gap-y-6 justify-center">
           {identityCards.map((identityCard, index) => (
             <IdentityCard key={index} {...identityCard} />
