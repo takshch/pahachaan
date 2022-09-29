d60fdd6fff8ce5ac3c0dc5d16c053e51941e8222
takshch <takshch089@gmail.com>
2022-09-29 23:59:56 +0530

    Adds Identities link in Navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
+
 function Navbar() {
   return (
     <div className="flex items-center px-5 py-2 bg-indigo-600 text-white border-none">
       <span className="mr-auto font-medium text-lg">Pahachaan</span>
       <nav>
         <ul className="flex items-center gap-6">
-          <li>Codes</li>
+          <li>
+            <Link href="/codes">
+              <a>Codes</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/identities">
+              <a>Identities</a>
+            </Link>
+          </li>
           <li>
             <button className="bg-purple-500 px-3 py-0.5 rounded">
               Logout
